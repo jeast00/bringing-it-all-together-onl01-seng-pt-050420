7edcdb9a59c83df0fdab2d0396eacd26932c4bc2
Jonathan East <jonathan_east@ymail.com>
2020-07-06 19:42:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   attr_accessor :name, :breed, :id
 
   def initialize(attributes, id = nil)
-    attributes.each { |k, v| self.send(("#{key}="), value)}
+    attributes.each { |k, v| self.send(("#{key}="), value) }
   end
 
 end
